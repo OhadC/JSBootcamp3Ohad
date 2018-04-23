14a65f759cf5c481201dacc84c3faccb7cf5de7b
Fulls5 <Fulls5@sqlabs.co.il>
2018-04-23 15:32:56 +0300

    fixing cube

--- 23.4/cube/main.js
+++ 23.4/cube/main.js
@@ -1,4 +1,4 @@
-rollDice = _ => {
+const rollDice = _ => {
     const rundomNumber = Math.floor(Math.random() * 10 % 6 + 1)
     const letters = ['a', 'b', 'c', 'd', 'e', 'f']
     return letters[rundomNumber]
