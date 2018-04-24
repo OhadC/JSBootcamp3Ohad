a8768d572f46d0338219b9a30e712255596ddaff
Ohad Cohen <ohadc.me@gmail.com>
2018-04-24 11:26:20 +0300

    humus chips salat

---
+++ Day 3 - 24.4/bitwise/bitwise-options.js
@@ -0,0 +1,38 @@
+var options = [
+    'hummus', //1
+    'hamutzim', //2
+    'salat', //4
+    'chips' //8
+];
+
+var bitwiseOptions = {};
+
+function buildOptions() {
+    for (var i=0; i<options.length; i++){
+        bitwiseOptions[options[i]] = 1 << i;
+    }
+}
+
+function is(obj, option) {
+    const optionValue = bitwiseOptions[option]
+    return (obj & optionValue) > 0
+}
+
+function set(obj, option) {
+    const optionValue = bitwiseOptions[option]
+    return obj | optionValue
+}
+
+var pita = 1;
+
+buildOptions();
+bitwiseOptions
+let answer = is(pita, 'hummus')
+answer
+answer = is(pita, 'salat')
+answer
+
+pita = set(pita, 'hummus')
+pita
+pita = set(pita, 'chips')
+pita
